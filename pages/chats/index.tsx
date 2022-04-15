aef578c1e302eca02311e9dfeca86bff498f1048
신현수 <sinhyeonsu@MacBook-Pro.local>
2022-04-15 14:10:59 +0900

    page add

---
+++ pages/chats/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import Layout from "../../components/layout";
+
+const Chats: NextPage = () => {
+	return (
+		<Layout hasTabBar title="채팅">
+			<div className="py-10 divide-y-[1px]">
+				{[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+					<div
+						key={i}
+						className="flex px-4  py-3 cursor-pointer items-center space-x-3"
+					>
+						<div className="w-10 h-10 rounded-full bg-slate-300" />
+						<div>
+							<p className="font-medium text-gray-700">Steve Jebs</p>
+							<p className="text-sm font-medium text-gray-500">
+								See you tomorrow in the corner at 2pm!
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</Layout>
+	);
+};
+
+export default Chats;
