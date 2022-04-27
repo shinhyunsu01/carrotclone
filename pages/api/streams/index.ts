120c1f7627f933cd781c920744a27818266b5323
신현수 <sinhyeonsu@MacBook-Pro.local>
2022-04-27 14:50:09 +0900

    cloudflare init

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -33,7 +33,10 @@ async function handler(
 		});
 	}
 	if (req.method === "GET") {
-		const streams = await client.stream.findMany({});
+		const streams = await client.stream.findMany({
+			take: 10,
+			skip: 30,
+		});
 		res.json({
 			ok: true,
 			streams,
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,7 +1,7 @@
 import useMutation from "@libs/client/useMutation";
 import useUser from "@libs/client/useUser";
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
@@ -11,6 +11,7 @@ interface EditProfileForm {
 	email?: string;
 	phone?: string;
 	name?: string;
+	avatar?: FileList;
 	formErrors?: string;
 }
 
@@ -27,6 +28,7 @@ const EditProfile: NextPage = () => {
 		handleSubmit,
 		setError,
 		formState: { errors },
+		watch,
 	} = useForm<EditProfileForm>();
 
 	useEffect(() => {
@@ -37,7 +39,7 @@ const EditProfile: NextPage = () => {
 
 	const [editProfile, { data, loading }] =
 		useMutation<EditPorfileResponse>(`/api/users/me`);
-	const onValid = ({ email, phone, name }: EditProfileForm) => {
+	const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
 		if (loading) return;
 		if (email === "" && phone === "" && name === "") {
 			return setError("formErrors", {
@@ -55,17 +57,37 @@ const EditProfile: NextPage = () => {
 			setError("formErrors", { message: data.error });
 		}
 	}, [data, setError]);
+
+	const [avatarPreview, setAvatarPreview] = useState("");
+	const avatar = watch("avatar");
+	useEffect(() => {
+		if (avatar && avatar.length > 0) {
+			const file = avatar[0];
+			setAvatarPreview(URL.createObjectURL(file));
+		}
+	}, [avatar]);
 	return (
 		<Layout canGoBack title="Edit Profile">
 			<form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
 				<div className="flex items-center space-x-3">
-					<div className="w-14 h-14 rounded-full bg-slate-500" />
+					{avatarPreview ? (
+						<img
+							src={avatarPreview}
+							className="w-14 h-14 rounded-full bg-slate-500"
+						/>
+					) : (
+						<div
+							src={avatarPreview}
+							className="w-14 h-14 rounded-full bg-slate-500"
+						/>
+					)}
 					<label
 						htmlFor="picture"
 						className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
 					>
 						Change
 						<input
+							{...register("avatar")}
 							id="picture"
 							type="file"
 							className="hidden"
